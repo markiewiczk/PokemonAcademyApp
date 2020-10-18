a4202d5d34f5a4527917ab38ce8ce2c3ac2d9f74
Karol Markiewicz <karolmarkiewicz0@gmail.com>
2020-10-18 15:34:47 +0200

    feature(paa): add pokemon details to details screen

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -2,13 +2,44 @@ import React from 'react';
 
 class PokemonDetails extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { PokemonDetails: null}
+
+    }
+
+
+    componentDidMount() {
+        const name = this.props.match.params.name;
+        fetch(`https://pokemonacademyapi-km.herokuapp.com/pokemons/${name}`)
+        .then(response => response.json()
+        .then(jsonResponse => {
+            console.log(jsonResponse);
+            this.setState({PokemonDetails: jsonResponse});
+        }))
+    }
+
+    renderPokemon = () => {
+        const { imageUrl, name, types, abilities, height, weight } = this.state.PokemonDetails;
+        return(
+            <div>
+                <h3>{`name: ${name}`}</h3>
+                <h3>{`types: ${types}`}</h3>
+                <h3>{`abilities: ${abilities}`}</h3>
+                <h3>{`height: ${height}`}</h3>
+                <h3>{`weight: ${weight}`}</h3>
+                <img src={imageUrl}/>  
+
+
+            </div>
+        )
+    }
+
     render() {
         return (
             <div>
                 <h1>Pokemon Details! </h1>
-                <h1>{this.props.match.params.name}</h1>
-
-
+                {this.state.PokemonDetails && this.renderPokemon()}
             </div>
         )
     }
